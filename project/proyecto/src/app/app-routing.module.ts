bab782e29b5ce53e2175bbf8a708e00efa2d84bd
Belaam042003 <isabella.ardila02@usa.edu.co>
2023-09-17 21:47:22 -0500

    Ya deje listo el front de ambos, se le hizo cambios al fondo a algunas caracteristicas de los botones para empezar con el backen ya que tuve inconvenientes con el pc

--- project/proyecto/src/app/app-routing.module.ts
+++ project/proyecto/src/app/app-routing.module.ts
@@ -10,30 +10,36 @@ import {
   redirectLoggedInTo,
 } from '@angular/fire/auth-guard';
 import { ProfileComponent } from './components/profile/profile.component';
-import { SubirDocsComponent } from './laura/components/subir-docs/subir-docs.component';
-import { RecursosCompartidosComponent } from './laura/components/recursos-compartidos/recursos-compartidos.component';
-import { InicialComponent } from './nicolas/perfil/inicial/inicial.component';
-import { InicioBusquedaComponent } from './paul/inicio-busqueda/inicio-busqueda.component';
-import { inicialResenasComponent } from './julieth/resenas/inicialResenas/inicialResenas.component';
-import { BlogComponent } from './guayara/blog/blog.component';
-import { HomeGComponent } from './guayara/homeG/homeG.component';
 
 const redirectToLogin = () => redirectUnauthorizedTo(['login']);
 const redirectToHome = () => redirectLoggedInTo(['home']);
 
 const routes: Routes = [
-  {path: '', pathMatch: 'full',component: LoginComponent,},
-  { path: 'recursos', component: RecursosCompartidosComponent,  }, 
-  { path: 'subirDocs', component: SubirDocsComponent, } ,
-  { path: 'perfil', component: InicialComponent,},
-  { path: 'busqueda', component: InicioBusquedaComponent,},
-  { path: 'resenas', component: inicialResenasComponent,},
-  { path: 'homeG', component: HomeGComponent},
-  { path: 'blog', component: BlogComponent},
-  { path: 'login',component: LoginComponent,...canActivate(redirectToHome),},
-  { path: 'sign-up',component: SignUpComponent,...canActivate(redirectToHome),},
-  { path: 'home',component: HomeComponent,...canActivate(redirectToLogin),},
-  { path: 'profile',component: ProfileComponent,...canActivate(redirectToLogin),},
+  {
+    path: '',
+    pathMatch: 'full',
+    component: LoginComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    ...canActivate(redirectToHome),
+  },
+  {
+    path: 'sign-up',
+    component: SignUpComponent,
+    ...canActivate(redirectToHome),
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    ...canActivate(redirectToLogin),
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    ...canActivate(redirectToLogin),
+  },
 ];
 
 @NgModule({
