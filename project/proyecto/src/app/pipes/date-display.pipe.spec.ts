3cf92985fc160f6a5503d372c43a4e5fce48c866
Samkter <paul.henao01@usa.edu.co>
2023-09-26 16:04:27 -0500

    se arregla el organizamiento de la lista

--- project/proyecto/src/app/pipes/date-display.pipe.spec.ts
+++ project/proyecto/src/app/pipes/date-display.pipe.spec.ts
@@ -1,3 +1,8 @@
+
+/*
+
+ESTO ES LO QUE ESTABA EN LA ORIGINAL
+
 import { DateDisplayPipe } from './date-display.pipe';
 
 describe('DateDisplayPipe', () => {
@@ -6,8 +11,15 @@ describe('DateDisplayPipe', () => {
     expect(pipe).toBeTruthy();
   });
 });
+*/
+
 
 /*
+
+
+Esto estaba en la mia 
+
+
 import { SearchPipe } from './search.pipe';
 
 describe('SearchPipe', () => {
