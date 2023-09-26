a0408a4a01e4fe8737515e4dffdefe7482694b4b
Samkter <paul.henao01@usa.edu.co>
2023-09-26 16:00:10 -0500

    se documenta el arreglo del boton que habia quedado sin acomodar de la mejor manera

--- project/proyecto/src/app/pipes/date-display.pipe.spec.ts
+++ project/proyecto/src/app/pipes/date-display.pipe.spec.ts
@@ -6,3 +6,15 @@ describe('DateDisplayPipe', () => {
     expect(pipe).toBeTruthy();
   });
 });
+
+/*
+import { SearchPipe } from './search.pipe';
+
+describe('SearchPipe', () => {
+  it('create an instance', () => {
+    const pipe = new SearchPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
+
+*/
--- project/proyecto/src/app/pipes/date-display.pipe.ts
+++ project/proyecto/src/app/pipes/date-display.pipe.ts
@@ -12,3 +12,19 @@ export class DateDisplayPipe implements PipeTransform {
     return this.datePipe.transform(date?.toMillis(), 'short') ?? '';
   }
 }
+/*
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'search'
+})
+export class SearchPipe implements PipeTransform {
+
+  transform(lista: any[], texto: string): any[] {
+    if (!texto) return lista
+    return lista.filter(user => user.name.toUpperCase().includes(texto.toUpperCase()))
+  }
+
+}
+
+*/
